e08dc9f14ba17b73e22bafe18d867cfcb34c3f79
Muhammed Kerem Kahraman <keremkahraman53@gmail.com>
2025-05-22 17:51:19 +0300

    typo in header include.

--- src/laserMapping.cpp
+++ src/laserMapping.cpp
@@ -52,7 +52,7 @@
 #include <tf2_ros/transform_broadcaster.h>
 
 #include <tf2/LinearMath/Quaternion.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 typedef pcl::PointXYZI PointType;
 
