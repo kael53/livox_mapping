cdc308e858bc6a0f0ca7bb6fc2eb0cdbcf799eee
Muhammed Kerem Kahraman <keremkahraman53@gmail.com>
2025-05-25 11:44:11 +0300

    Updated launch file and added urdfs to cmake install

--- launch/robot.launch.py
+++ launch/robot.launch.py
@@ -4,11 +4,6 @@ from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
- #   urdf_path = os.path.join(
- #       get_package_share_directory('livox_mapping'),
- #       'urdf',
- #       'g1_29dof_lock_waist_with_hand_rev_1_0.urdf'
- #   )
     urdf_path = os.path.join(
         get_package_share_directory('livox_mapping'),
         'robots',
